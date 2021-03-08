f689895c336763218bcb25a1fb0d6fe99738b944
Andre Anggono <andre.anggono@hotmail.com>
2021-03-08 13:01:50 +1100

    Added auth components

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,35 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Header from './Header'
-import Home from './Home';
-import ProductsIndex from './ProductsIndex';
-import ProductShow from './ProductShow';
-import Sidebar from './Sidebar';
-import UserShow from './UserShow';
-import CheckoutCart from './CheckoutCart';
+import Header from "./Header";
+import Home from "./Home";
+import ProductsIndex from "./ProductsIndex";
+import ProductShow from "./ProductShow";
+import Sidebar from "./Sidebar";
+import UserShow from "./UserShow";
+import CheckoutCart from "./CheckoutCart";
+import Login from "./auth/Login";
 
 function App(props) {
-
 	return (
 		<>
 			<BrowserRouter>
-				<Header/>
-				<Sidebar/>
+				<Header />
+				<Sidebar />
 				<div className="container">
 					<Switch>
+						<Route exact path="/" component={() => <Home />} />
 
-						<Route exact path='/' component={() => <Home/>} />
-
-						<Route exact path='/products' component={(props) => <ProductsIndex {...props} />} />
-						<Route exact path='/product/:product_id' component={(props) => <ProductShow {...props}/>} />
-						<Route exact path='/checkout' component={(props) => <CheckoutCart {...props}/>} />
-
-						<Route exact path='/user/:user_id' component={() => <UserShow/>} />
-
+						<Route exact path="/products" component={(props) => <ProductsIndex {...props} />} />
+						<Route exact path="/product/:product_id" component={(props) => <ProductShow {...props} />} />
+						<Route exact path="/checkout" component={(props) => <CheckoutCart {...props} />} />
+						<Route path="/login" component={(props) => <Login {...props} />} />
+						<Route exact path="/user/:user_id" component={() => <UserShow />} />
 					</Switch>
 				</div>
 			</BrowserRouter>
 		</>
-	) 
+	);
 }
 
 export default App;

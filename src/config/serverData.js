53ac7a45b0442a2e0676d7eaced9c253810a2446
Andre Anggono <andre.anggono@hotmail.com>
2021-03-09 17:03:50 +1100

    staging for production

--- src/config/serverData.js
+++ src/config/serverData.js
@@ -1,5 +1,11 @@
-export const ROOT = "http://localhost:3000";
-export const LOGGED_IN = "http://localhost:3000/isloggedin";
-export const LOGIN = "http://localhost:3000/login";
-export const LOGOUT = "http://localhost:3000/logout";
-export const REGISTER = "http://localhost:3000/users";
+// export const ROOT = "http://localhost:3000";
+// export const LOGGED_IN = "http://localhost:3000/isloggedin";
+// export const LOGIN = "http://localhost:3000/login";
+// export const LOGOUT = "http://localhost:3000/logout";
+// export const REGISTER = "http://localhost:3000/users";
+
+export const ROOT = "https://furnique.herokuapp.com";
+export const LOGGED_IN = "https://furnique.herokuapp.com/isloggedin";
+export const LOGIN = "https://furnique.herokuapp.com/login";
+export const LOGOUT = "https://furnique.herokuapp.com/logout";
+export const REGISTER = "https://furnique.herokuapp.com/users";

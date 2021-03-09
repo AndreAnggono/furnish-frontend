988c7bdaaa88f8f40ff69f81d7989ded6ee768cd
Andre Anggono <andre.anggono@hotmail.com>
2021-03-09 17:57:46 +1100

    fixed prod bug

--- src/components/ProductShow.js
+++ src/components/ProductShow.js
@@ -1,61 +1,57 @@
-import axios from 'axios';
-import React from 'react';
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-import AddToCart from './AddToCart';
-import RemoveFromCart from './RemoveFromCart';
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { PRODUCTS } from "../config/serverData";
+import AddToCart from "./AddToCart";
+import RemoveFromCart from "./RemoveFromCart";
 
+function ProductShow({ match }) {
+	// const [product, setProduct] = useState('');
 
-function ProductShow({match}) {
-    // const [product, setProduct] = useState('');
+	// useEffect(() => {
+	//     const productURL = (id) => {
+	//         return `/products/${id}`
+	//     }
+	//     const id = match.params.product_id;
 
-    // useEffect(() => {
-    //     const productURL = (id) => {
-    //         return `/products/${id}`
-    //     }
-    //     const id = match.params.product_id;
-        
-    //     async function fetchProduct () {
-    //     const response = await axios.get(productURL(id));
-    //     // productObj = response.data;
-    //     setProduct(response.data);
-    //     }
-    //     fetchProduct();
-    // }, []);
+	//     async function fetchProduct () {
+	//     const response = await axios.get(productURL(id));
+	//     // productObj = response.data;
+	//     setProduct(response.data);
+	//     }
+	//     fetchProduct();
+	// }, []);
 
+	// if (!product) {
+	//     return null;
+	// }
 
-    // if (!product) {
-    //     return null;
-    // }
+	// @ts-ignore
+	const { product_id } = useParams();
+	const { data: product, isLoading, isError, error } = useQuery(["Product", product_id], () => axios(`${PRODUCTS}/${product_id}`).then((res) => res.data));
 
-    // @ts-ignore
-    const {product_id} = useParams();
-    const {data: product, isLoading, isError, error } = useQuery(["Product", product_id], () => axios(`http://localhost:3000/products/${product_id}`).then(res => res.data));
+	if (isLoading) return <h1>Loading...</h1>;
+	if (isError) return <h1>{error}</h1>;
 
-    if (isLoading) return <h1>Loading...</h1>
-    if (isError) return <h1>{error}</h1>
+	// @ts-ignore
+	const { name, description, price, color, style, categories, image } = product;
+	const showCategories = categories.map((category) => <li key={category}>{category}</li>);
 
+	return (
+		<>
+			<h1>{name}</h1>
+			<img className="product__image" src={image} alt="" />
 
-    // @ts-ignore
-    const {name, description, price, color, style, categories, image} = product;
-    const showCategories = categories.map(category => (
-        <li key={category}>{category}</li>
-    ))
-
-    return (
-        <>
-            <h1>{name}</h1>
-            <img className="product__image" src={image} alt=""/>
-
-            <p>Description: {description}</p>
-            <p>Price: ${price}</p>
-            <p>Color: {color}</p>
-            <p>Style: {style}</p>
-            <ul>Categories: {showCategories}</ul>
-            <AddToCart product={product}/>
-            <RemoveFromCart product={product}/>
-        </>
-    )
+			<p>Description: {description}</p>
+			<p>Price: ${price}</p>
+			<p>Color: {color}</p>
+			<p>Style: {style}</p>
+			<ul>Categories: {showCategories}</ul>
+			<AddToCart product={product} />
+			<RemoveFromCart product={product} />
+		</>
+	);
 }
 
-export default ProductShow
+export default ProductShow;
--- src/utils/useCheckout.js
+++ src/utils/useCheckout.js
@@ -2,25 +2,27 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useShoppingCart } from "use-shopping-cart";
 import { stripePromise } from "../components/App";
+import { CHECKOUT } from "../config/serverData";
 
 function useCheckout() {
-    const {cartDetails} =  useShoppingCart();
+	const { cartDetails } = useShoppingCart();
 
-    async function _handleCheckout() {
-        const session = await axios.post('http://localhost:3000/checkout-sessions', cartDetails)
-            .then(res => res.data)
-            .catch(error => {
-                toast.error('Checkout failed!');
-                console.log('Error during checkout: ', error);
-            })
+	async function _handleCheckout() {
+		const session = await axios
+			.post(CHECKOUT, cartDetails)
+			.then((res) => res.data)
+			.catch((error) => {
+				toast.error("Checkout failed!");
+				console.log("Error during checkout: ", error);
+			});
 
-        if (session) {
-            console.log(session);
-            const stripeObj = await stripePromise;
-            stripeObj.redirectToCheckout({sessionId: session.session.id});
-        }
-    }
-    return _handleCheckout;
+		if (session) {
+			console.log(session);
+			const stripeObj = await stripePromise;
+			stripeObj.redirectToCheckout({ sessionId: session.session.id });
+		}
+	}
+	return _handleCheckout;
 }
 
 export default useCheckout;

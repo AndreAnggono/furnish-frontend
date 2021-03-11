e2b2c956aa7130371abb544046c98c124a957141
Mai Nguyen <chimai.dk@gmail.com>
2021-03-11 22:19:23 +1100

    Css-ing still

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,11 @@
 
 function Footer() {
     return(
-        <>
-            <section className="footer">
-				<ul class="nav">
-						
-						<li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
-						<li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
-						<li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
-						<li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
-
-				</ul>
-				<p className="copyright">
-					&copy; Copyright 2021 by Furnique Australia
-				</p>
-			</section>
-
-        </>
+        <div className="footer">
+            <p className="copyright">
+                &copy; Copyright 2021 by Furnique Australia
+            </p>
+        </div>
     )
 }
 
